30439839a33cef8cc5bac08ccc8646c9e169a4a8
Edward <ed.f.jones@gmail.com>
2023-04-08 13:33:41 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(skate = "roller-skate") {
+    return `This Saturday, I want to ${skate}!`
+}
+
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(string) {
+    return function word(phrase) {
+      return `You are ${string}${phrase}${string}!`  
+    }
+}
+
+wrapAdjective("*")("a dedicated progrmmer!")
